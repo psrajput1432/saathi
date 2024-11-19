bca6f1fce8995318721e4505fc9c96daaf111e79
psrajput1432 <pallavi.singh@iic.ac.in>
2024-11-20 02:09:33 +0530

    Added sidebar and Background

--- extend.js
+++
@@ -1,8 +0,0 @@
-// document.getElementById("toggleButton").addEventListener("click", function () {
-//   const sidebar = document.getElementById("sidebar");
-//   if (sidebar.style.left === "0px") {
-//     sidebar.style.left = "-250px"; // Hide the sidebar
-//   } else {
-//     sidebar.style.left = "0px"; // Show the sidebar
-//   }
-// });
